ebb21b600d1c94b1d402f5e77a228dba6db67fd1
Diego Cumplido <137076465+BODYSN4TCHER00@users.noreply.github.com>
2025-03-18 03:51:15 -0600

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, updateUser, deleteUser } = require("../controllers/authController");
+const { registerUser, loginUser, updateUser, deleteUser, getAllUsers } = require("../controllers/authController");
 
 // Registrar un usuario
 router.post("/register", registerUser);
 
 // Iniciar sesión (login)
 router.post("/login", loginUser);
+
+// Obtener todos los usuarios
+router.get("/users", getAllUsers);
 
 // Actualizar un usuario
 router.put("/update/:id", updateUser);
