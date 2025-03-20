0a9e7ca1ce9fca51064a20331c1fd1364482d321
BODYSN4TCHER00 <dcumplido04@gmail.com>
2025-03-19 23:06:07 -0600

    Deploy y conexion con Atlas

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/asistenciaDB', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error('MongoDB connection error:', err);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Detener la aplicación si no hay conexión a la base de datos
+  }
 };
 
 module.exports = connectDB;
